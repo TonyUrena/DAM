394e2e0d60428dc8f4fa43944b81940582133607
hola <hola>
2022-12-04 21:58:28 +0100

    Terminado ejercicio star wars

--- PROGRAMACION/05/ejercicios/StarWars.java
+++ PROGRAMACION/05/ejercicios/StarWars.java
@@ -1,4 +1,7 @@
-import java.util.Random;
+/*
+ * TONY UREÑA FORTUÑO
+ */
+
 import java.util.Scanner;
 
 public class StarWars {
@@ -7,11 +10,11 @@ public class StarWars {
 
         Scanner reader = new Scanner(System.in);
         
-        String textoInicio, textoNivel1, textoNivel2,
-        textoNivel3, textoNivel4, textoNivel5, textoGanar,
-        textoPerder, textoFin;
+        String textoNivel1, textoNivel2, textoNivel3,
+        textoNivel4, textoNivel5, textoTeclaIntro,
+        textoInicio, textoGanar, textoPerder, textoFin,
+        borrarTerminal;
 
-        Random random = new Random();
         String teclaIntro = "a";
         int numeroS1, numeroS2, sumatorioS1S2 = 0, solucionNivel1;
         int numeroP1, numeroP2, productorioP1P2 = 1, solucionNivel2 ;
@@ -94,46 +97,47 @@ public class StarWars {
             //System.out.println(factorialS);
             
         }
-
         sumaFactoriales = factorialM + factorialS;
 
 
-
         // Inicialización de textos
-        textoInicio = "=== STAR WARS CÓDIGOS SECRETOS ===\n Hace mucho tiempo, en una galaxia muy, muy lejana … La Princesa Leia, LukeSkywalker, Han Solo, Chewbacca, C3PO y R2D2 viajan en una nave imperial robada en una misión secreta para infiltrarse en otra estrella de la muerte que el imperio está construyendo para destruirla. (Presiona Intro para continuar)";
-        textoNivel1 = "Los problemas empiezan cuando deben realizar un salto hiperespacial hasta al sistema " + numeroS1 + " en el sector" + numeroS2 +", pero el sistema de navegación está estropeado y el computador tiene problemas para calcular parte de las coordenadas de salto. Chewbacca, piloto experto, se da cuenta que falta el cuarto número de la serie. Recuerda de sus tiempos en la academia de pilotos que para calcularlo hay que calcular el sumatorio entre el nº del sistema y el nº del sector (ambos inclusive).\n ¿Qué debe introducir?";
-        textoNivel2 = "Gracias a Chewbacca consiguen llegar al sistema correcto y ven a lo lejos la estrella de la muerte. Como van en una nave imperial robada se aproximan lentamente con la intención de pasar desapercibidos. De repente suena el comunicador. “Aquí agente de espaciopuerto " + numeroP1 + " contactando con nave imperial " + numeroP2 + ". No están destinados en este sector. ¿Qué hacen aquí?”. Han Solo coge el comunicador e improvisa. “Eh … tenemos un fallo en el … eh … condensador de fluzo... Solicitamos permiso para atracar y reparar la nave”. El agente, que no se anda con tonterías, responde “Proporcione código de acceso o abriremos fuego”. Han Solo ojea rápidamente el manual del piloto que estaba en la guantera y da con la página correcta. El código es el productorio entre el nº del agente y el nº de la nave (ambos inclusive). ¿Cuál es el código?";
-        textoNivel3 = "Han Solo proporciona el código correcto. Atracan en la estrella de la muerte, se equipan con trajes de soldados imperiales que encuentran en la nave para pasar desapercibidos y bajan. Ahora deben averiguar en qué nivel de los " + numeroN + " existentes se encuentra el reactor principal. Se dirigen al primer panel computerizado que encuentran y la Princesa Leia intenta acceder a los planos de la nave pero necesita introducir una clave de acceso. Entonces recuerda la información que le proporcionó Cando Falrissian “La clave de acceso a los planos de la nave es el factorial de N/10 (redondeando N hacia abajo), donde N es el no de niveles”. ¿Cual es el nivel correcto?";
-        textoNivel4 = "Gracias a la inteligencia de Leia llegan al nivel correcto y encuentran la puerta acorazada que da al reactor principal. R2D2 se conecta al panel de acceso para intentar hackear el sistema y abrir la puerta. Para desencriptar la clave necesita verificar si el número " + numeroP + " es primo o no. Si es primo introduce un 1, si no lo es introduce un 0.";
-        textoNivel5 = "Consiguen entrar al reactor. Ya solo queda que Luke Skywalker coloque la bomba, programe el temporizador y salir de allí corriendo. Necesita programarlo para que explote en exactamente ​M minutos y ​S segundos, el tiempo suficiente para escapar antes de que explote pero sin que el sistema de seguridad anti-explosivos detecte y desactive la bomba. Pero el temporizador utiliza un reloj Zordgiano un tanto peculiar. Para convertir los minutos y segundos al sistema Zordgiano hay que sumar el factorial de M y el factorial de S. ¿Qué valor debe introducir?";
-        textoGanar  = "Luke Skywalker introduce el tiempo correcto, activa el temporizador y empiezan a sonar las alarmas. Salen de allí corriendo, no hay tiempo que perder. La nave se convierte en un hervidero de soldados de arriba a abajo y entre el caos que les rodea consiguen llegar a la nave y salir de allí a toda prisa. A medida que se alejan observan por la ventana la imagen de la colosal estrella de la muerte explotando en el silencio del espacio, desapareciendo para siempre junto a los restos del malvado imperio. ¡Has salvado la galaxia gracias a la Fuerza Jedi de las matemáticas! Enhorabuena ;D";
-        textoPerder = "Ese no era el código correcto... La misión ha sido un fracaso... :( :( :( Todavía no eres un Maestro Jedi de las Matemáticas. ¡Vuelve a intentarlo!";
-        textoFin    = "Gracias por jugar :D";
+        // \033[H\033[2J es el código ANSI para borrar la pantalla del terminal.
+        borrarTerminal = "\f";
+        textoInicio = "=== STAR WARS CÓDIGOS SECRETOS ===\n\nHace mucho tiempo, en una galaxia muy, muy lejana…\n\nLa Princesa Leia, Luke Skywalker, Han Solo, Chewbacca, C3PO y R2D2 viajan\nen una nave imperial robada en una misión secreta para infiltrarse en otra\nestrella de la muerte que el imperio está construyendo para destruirla.\n\n(Presiona Intro para continuar)";
+        textoTeclaIntro = "La princesa Leia te dirige una mirada de reojo. Saca un cuaderno y un\nbolígrafo de un bolsillo oculto en su toga intergaláctica y escribe una nota:\n\n    Requisitos para próximos agentes:\n       -Nivel de matemáticas: 4º ESO\n       -Nivel de comprensión lectora: 2º Parvulario.\n\nCierra su bloc de notas y lo guarda junto al bolígrafo. Te mira fijamente y repite:\n\n(Presiona Intro para continuar)";
+        textoNivel1 = "Los problemas empiezan cuando deben realizar un salto hiperespacial hasta\nel sistema " + numeroS1 + " en el sector " + numeroS2 +", pero el sistema de navegación está estropeado\ny el computador tiene problemas para calcular parte de las coordenadas de salto.\n\nChewbacca, piloto experto, se da cuenta que falta el cuarto número de la serie.\n\nRecuerda de sus tiempos en la academia de pilotos que para calcularlo hay que\ncalcular el sumatorio entre el nº del sistema y el nº del sector (ambos inclusive).\n\n¿Qué debe introducir?\n";
+        textoNivel2 = "Gracias a Chewbacca consiguen llegar al sistema correcto y ven a lo lejos\nla estrella de la muerte.\n\nComo van en una nave imperial robada se aproximan lentamente con la intención\nde pasar desapercibidos.\n\nDe repente suena el comunicador: “Aquí agente de espaciopuerto " + numeroP1 + " contactando\ncon nave imperial " + numeroP2 + ". No están destinados en este sector. ¿Qué hacen aquí?”.\n\nHan Solo coge el comunicador e improvisa. “Eh… tenemos un fallo en el… eh…\ncondensador de fluzo... Solicitamos permiso para atracar y reparar la nave”.\n\nEl agente, que no se anda con tonterías, responde “Proporcione código de\nacceso o abriremos fuego”.\n\nHan Solo ojea rápidamente el manual del piloto que estaba en la guantera\ny da con la página correcta. El código es el productorio entre el nº del\nagente y el nº de la nave (ambos inclusive).\n\n¿Cuál es el código?\n";
+        textoNivel3 = "Han Solo proporciona el código correcto.\n\nAtracan en la estrella de la muerte, se equipan con trajes de soldados\nimperiales que encuentran en la nave para pasar desapercibidos y bajan.\n\nAhora deben averiguar en qué nivel de los " + numeroN + " existentes se encuentra el\nreactor principal.\n\nSe dirigen al primer panel computerizado que encuentran y la Princesa Leia\nintenta acceder a los planos de la nave pero necesita introducir una clave\nde acceso.\n\nEntonces recuerda la información que le proporcionó Cando Falrissian\n“La clave de acceso a los planos de la nave es el factorial de N/10\n(redondeando N hacia abajo), donde N es el nº de niveles”.\n\n¿Cual es el nivel correcto?\n";
+        textoNivel4 = "Gracias a la inteligencia de Leia llegan al nivel correcto y encuentran la\npuerta acorazada que da al reactor principal. R2D2 se conecta al panel de\nacceso para intentar hackear el sistema y abrir la puerta.\n\nPara desencriptar la clave necesita verificar si el número " + numeroP + " es primo o no.\n\nSi es primo introduce un 1, si no lo es introduce un 0.\n";
+        textoNivel5 = "Consiguen entrar al reactor. Ya solo queda que Luke Skywalker coloque\nla bomba, programe el temporizador y salir de allí corriendo.\n\nNecesita programarlo para que explote en exactamente " + numeroMinutos + " minutos y " + numeroSegundos + " segundos,\nel tiempo suficiente para escapar antes de que explote pero sin que el sistema\nde seguridad anti-explosivos detecte y desactive la bomba.\n\nPero el temporizador utiliza un reloj Zordgiano un tanto peculiar.\n\nPara convertir los minutos y segundos al sistema Zordgiano hay que sumar el\nfactorial de " + numeroMinutos + " y el factorial de " + numeroSegundos + ".\n\n¿Qué valor debe introducir?\n";
+        textoGanar  = "Luke Skywalker introduce el tiempo correcto, activa el temporizador y empiezan\na sonar las alarmas. Salen de allí corriendo, no hay tiempo que perder.\n\nLa nave se convierte en un hervidero de soldados de arriba a abajo y entre el\ncaos que les rodea consiguen llegar a la nave y salir de allí a toda prisa.\n\nA medida que se alejan observan por la ventana la imagen de la colosal estrella\nde la muerte explotando en el silencio del espacio, desapareciendo para siempre\njunto a los restos del malvado imperio.\n\n¡Has salvado la galaxia gracias a la Fuerza Jedi de las matemáticas!\n\nEnhorabuena ;D";
+        textoPerder = "Ese no era el código correcto...\n\nLa misión ha sido un fracaso... :( :( :(\n\nTodavía no eres un Maestro Jedi de las Matemáticas.\n\n¡Vuelve a intentarlo!";
+        textoFin    = "Gracias por jugar :D\n\n";
 
     /////
     // INICIO
     //////////////
 
+    System.out.print(borrarTerminal);
     System.out.println(textoInicio);
 
-   while (!teclaIntro.equals("")){
-    teclaIntro = reader.nextLine();
-    if (!teclaIntro.equals("")){
-        System.out.println("\nLa princesa Leia te dirige una mirada de reojo. Saca un cuaderno y un ");
-        System.out.println("bolígrafo de un bolsillo oculto en su toga intergaláctica y escribe una nota:\n");
-        System.out.println("    Requisitos para próximos agentes: ");
-        System.out.println("       -Nivel de matemáticas: 4º ESO");
-        System.out.println("       -Nivel de comprensión lectora: 2º Parvulario.");
-        System.out.println("\nCierra su bloc de notas y lo guarda junto al bolígrafo. Te mira fijamente y repite:");
-        System.out.println("\n(Presiona Intro para continuar)");
-    }
-    
+    while (!teclaIntro.equals("")){
+
+        teclaIntro = reader.nextLine();
+
+        if (!teclaIntro.equals("")){
+
+            System.out.println(textoTeclaIntro);
+
+        }
+
     };
 
     /////
     // Nivel 1
     /////////////
 
+        System.out.print(borrarTerminal);
         System.out.println(textoNivel1);
         // Debug muestra numeros aleatorio sumatorio
         System.out.println(numeroS1 + "-" + numeroS2 + " Sumatorio = " + sumatorioS1S2);
@@ -148,6 +152,7 @@ public class StarWars {
     // Nivel 2
     /////////////
 
+        System.out.print(borrarTerminal);
         System.out.println(textoNivel2);
         // Debug muestra numeros aleatorios productorio
         System.out.println(numeroP1 + "-" + numeroP2 + " Productorio = " + productorioP1P2);
@@ -161,7 +166,8 @@ public class StarWars {
     /////
     // Nivel 3
     /////////////
-
+        
+        System.out.print(borrarTerminal);
         System.out.println(textoNivel3);
         // Debug muestra numero aletorio / 10 factorial
         System.out.println("Factorial de " + numeroN + ": " + factorialN10 );
@@ -176,6 +182,7 @@ public class StarWars {
     // Nivel 4
     /////////////
 
+        System.out.print(borrarTerminal);
         System.out.println(textoNivel4);
         // Debug muestra si es primo
         System.out.println("El número " + numeroP + "es primo?: " + numeroPEsPrimo);
@@ -190,6 +197,7 @@ public class StarWars {
     // Nivel 5
     /////////////
 
+        System.out.print(borrarTerminal);
         System.out.println(textoNivel5);
         // Debug muestra la suma de los factoriales
         System.out.println("La suma de los factoriales es: " + sumaFactoriales);
@@ -203,7 +211,8 @@ public class StarWars {
     /////
     // GANAR
     /////////////
-
+        
+        System.out.print(borrarTerminal);
         System.out.println(textoGanar);
 
         }}}}}
@@ -211,7 +220,8 @@ public class StarWars {
     /////
     // FIN
     /////////////
-
+        
+        System.out.print(borrarTerminal);
         System.out.println(textoFin);
     }
 }
